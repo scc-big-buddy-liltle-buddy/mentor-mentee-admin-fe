ddaf49571cfbdf7285efbfd3c0b70caa537668d4
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-09-14 21:07:28 +0700

    U

--- src/component/MatchDetail.jsx
+++ src/component/MatchDetail.jsx
@@ -1,6 +1,44 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Center,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  IconButton,
+  Image,
+  Stack,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
+
+function Example() {
+  let [value, setValue] = React.useState("");
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value;
+    setValue(inputValue);
+  };
+  return (
+    <>
+      <Text mb="8px">Value: {value}</Text>
+      <Textarea
+        value={value}
+        onChange={handleInputChange}
+        placeholder="Here is a sample placeholder"
+        size="sm"
+      />
+    </>
+  );
+}
 
 const MatchDetail = () => {
   return (
@@ -11,7 +49,27 @@ const MatchDetail = () => {
       templateColumns="repeat(5, 1fr)"
       gap={4}
     >
-      <GridItem colSpan={2} bg="papayawhip" />
+      <GridItem
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        colSpan={2}
+        bg="tomato"
+      >
+        <Card maxW="sm">
+          <CardBody>
+            <Stack mt="6" spacing="3">
+              <Heading size="md">Name</Heading>
+              <Text>Location</Text>
+              <Text>
+                <Example />
+              </Text>
+              <Text>Price</Text>
+            </Stack>
+          </CardBody>
+          <Divider />
+        </Card>
+      </GridItem>
       <GridItem colSpan={3} bg="tomato" />
     </Grid>
   );
